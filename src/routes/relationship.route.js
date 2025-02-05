3eaf9de4b212459f18d4c849a97249cdf218abf6
Hải Tân <221124022310@due.udn.vn>
2025-02-05 10:47:29 +0700

    Hai commit v25

--- src/routes/relationship.route.js
+++ src/routes/relationship.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { blockUser } from "../controllers/relationship.controller.js";
+import { blockUser, getBlockedUsers, unblockUser } from "../controllers/relationship.controller.js";
 
 const router = express.Router();
 
+router.get("/blocked-users", protectRoute, getBlockedUsers);
+
 router.post("/block-user", protectRoute, blockUser);
+
+router.delete("/unblock-user", protectRoute, unblockUser);
 
 export default router;
